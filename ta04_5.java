8ec8c002569e8a41f2c87cf140d6168b66dcc549
fcarri <fcs0000@gmail.com>
2022-10-04 17:02:06 +0200

    Commit 8

---
+++ ta04_5.java
@@ -0,0 +1,27 @@
+package project8;
+
+public class ta04_5 {
+	public static void main(String args[]){
+		
+		int A=0;
+		int B=1;
+		int C=2;
+		int D=3;
+		
+		//B coge el valor de C
+		B=C;
+		System.out.println("B = "+B);
+		
+		//C  coge el valor de A
+		C=A;
+		System.out.println("C = "+C);
+		
+		//A coge el valor de D
+		A=D;
+		System.out.println("A = "+A);
+		
+		//D coge el valor de B
+		D=B;
+		System.out.println("D = "+D);
+	}
+}
